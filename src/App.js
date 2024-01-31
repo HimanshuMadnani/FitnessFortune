f77de63c7bb5b7285d825f1a80a666d826547d1b
HimanshuMadnani <himanshu.madnani22@gmail.com>
2024-01-31 12:03:36 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {BrowserRouter as Router, Route , Routes } from 'react-router-dom';
+import { Route , Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
@@ -15,12 +15,10 @@ const App = () => {
     <Box width = "400px" sx ={ {width: {xl: '1488px'}}} m="auto" >
     
     <Navbar />
-    <Router basename={`${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/exercise/:id" element={<ExcreciseDtail />}/>
       </Routes>
-    </Router>
     <Footer  /> 
     </Box>
   )
